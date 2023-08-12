ab57f76b94df890492da5da7932f9e3db47bac37
dittops <dittops@gmail.com>
2023-08-12 12:57:09 +0530

    add server apis

---
+++ src/server/constant.py
@@ -0,0 +1,9 @@
+import os
+import pymongo
+
+
+# Connect to MongoDB
+
+# client = pymongo.MongoClient(os.environ['DATABASE_URL'])
+# db = client["llm_training"]
+# pipeline_collection = db["pipeline"]
